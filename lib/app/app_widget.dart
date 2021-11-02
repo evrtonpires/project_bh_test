e2bf638d86394fc07ff51ee69bd668f999317be3
Everton Pires <everton.eduardo@caminoeducation.com>
2021-11-02 18:21:33 -0300

    feature(project_bh_test): improvement in app widget class

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -5,8 +5,9 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Slidy',
+      title: 'Projeto de Avaliação',
       theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
     ).modular();
   }
 }

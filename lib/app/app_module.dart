4c4275bfb2d145e0fdaeaeb36dff54afa91d23ad
Everton Pires <everton.eduardo@caminoeducation.com>
2021-11-02 18:15:40 -0300

    feature(project_bh_test): implementing dependency injection

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,14 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'modules/home/home_module.dart';
+import 'modules/home/home_store.dart';
+import 'modules/shared/auth/auth_controller.dart';
+import 'modules/shared/auth/auth_repository.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind<AuthRepository>((i) => AuthRepository(dio: i.get())),
+    Bind((i) => AuthController()),
+    Bind((i) => Dio()),
+    Bind((i) => HomeStore()),
+  ];
 
   @override
   final List<ModularRoute> routes = [
     ModuleRoute(Modular.initialRoute, module: HomeModule()),
   ];
-
 }
